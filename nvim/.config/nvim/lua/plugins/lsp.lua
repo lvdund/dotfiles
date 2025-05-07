1a46fcecd893afa3f8bff185c32165c5a3a36524
lvdund <lvdund@gmail.com>
2025-05-07 12:45:41 +0700

    edit: lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -222,16 +222,12 @@ return {
 						fallback()
 					end, { "i" }), -- Select the [n]ext item
 
-					-- Select the [n]ext item
-					-- ['<C-n>'] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
-					-- ['<C-p>'] = cmp.mapping.select_prev_item(),
-
 					-- Scroll the documentation window [b]ack / [f]orward
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-b>"] = cmp.mapping.scroll_docs(-3),
+					["<C-f>"] = cmp.mapping.scroll_docs(3),
 
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 					["<Tab>"] = cmp.mapping.select_next_item(),
 					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 					["<C-Space>"] = cmp.mapping.complete({}),
