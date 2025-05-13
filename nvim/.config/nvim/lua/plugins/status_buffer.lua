8bfa78dac566b477ea76f81eaab012901220ca6d
lvdund <lvdund@gmail.com>
2025-05-14 02:53:40 +0700

    edit: nvim status + i3 + kitty

--- nvim/.config/nvim/lua/plugins/status_buffer.lua
+++ nvim/.config/nvim/lua/plugins/status_buffer.lua
@@ -108,8 +108,8 @@ return {
 					lualine_a = { "mode" },
 					lualine_b = { "branch", "diff", "diagnostics" },
 					lualine_c = { "filename" },
-					lualine_x = { "encoding", "fileformat", "filetype" },
-					lualine_y = { "progress" },
+					lualine_x = { "filetype", "diff" },
+					lualine_y = { "datetime" },
 					lualine_z = { "location" },
 				},
 				inactive_sections = {
