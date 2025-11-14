0eb668367f8d4692c07b35fc6d0d80af692d4a2b
lvdund <lvdund@gmail.com>
2025-11-14 09:41:33 +0700

    neotree + fzf: root dir + qf

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -9,6 +9,7 @@ return {
   lazy = false,
   keys = {
     { '\\', '<Cmd>Neotree position=float reveal_force_cwd<CR>', desc = 'Neotree toggle', silent = true },
+    { '<C-\\>', '<Cmd>Neotree position=left reveal_force_cwd<CR>', desc = 'Neotree', silent = true },
   },
   opts = {
     default_component_configs = {
@@ -18,13 +19,13 @@ return {
       git_status = {
         symbols = {
           -- Change type
-          added = '✚ ',
-          modified = '',
-          deleted = '✖ ',
+          added = '󰷬 ',
+          modified = '󰲶 ',
+          deleted = '󰷩 ',
           renamed = '󰁕',
           -- Status type
           untracked = '',
-          ignored = ' ',
+          ignored = ' ',
           unstaged = '✗',
           staged = ' ',
           conflict = ' ',
@@ -54,6 +55,11 @@ return {
         enabled = true,
         leave_dirs_open = true,
       },
+      bind_to_cwd = true,
+      cwd_target = {
+        sidebar = 'global',
+        current = 'global',
+      },
     },
     commands = {
       -- -- Override delete to use trash instead of rm
@@ -72,4 +78,34 @@ return {
       -- end,
     },
   },
+  -- Set up autocommand to change to project root
+  config = function(_, opts)
+    -- Function to find git root
+    local function get_git_root()
+      local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
+      if vim.v.shell_error == 0 and git_root then
+        return git_root
+      end
+      return nil
+    end
+
+    -- Auto change to git root when opening files
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('NeoTreeGitRoot', { clear = true }),
+      pattern = '*',
+      callback = function()
+        -- Skip if neo-tree buffer
+        if vim.bo.filetype == 'neo-tree' then
+          return
+        end
+
+        local git_root = get_git_root()
+        if git_root and vim.fn.getcwd() ~= git_root then
+          vim.cmd('cd ' .. git_root)
+        end
+      end,
+    })
+
+    require('neo-tree').setup(opts)
+  end,
 }
